0ee66416f7d623c63acb168a3d57f26f79e2874c
Ramón Díaz <jramondiaz96@gmail.com>
2019-11-07 13:43:14 -0600

    Revert "tabla archivos_detalles"
    This reverts commit e23686557d50bf30d16ce3da1d8217a621badbb7.

--- resources/db/soccerMoms.sql
+++ resources/db/soccerMoms.sql
@@ -70,19 +70,6 @@ CREATE TABLE IF NOT EXISTS jugadoras (
 COLLATE='utf8_bin'
 ;
 
-DROP TABLE IF EXISTS `archivo_detalles`;
-CREATE TABLE IF NOT EXISTS `archivo_detalles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_jugadora` int(11) NOT NULL,
-  `archivo` varchar(255) COLLATE utf8_bin NOT NULL,
-  `status` int(11) NOT NULL DEFAULT '1',
-  `creacion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `actualización` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `FK_archivo_jugadora` (`id_jugadora`),
-  CONSTRAINT `FK_archivo_jugadora` FOREIGN KEY (`id_jugadora`) REFERENCES `jugadoras` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
 
 
 
@@ -98,26 +85,3 @@ ALTER TABLE equipos
   ADD CONSTRAINT FK_equipo_usuario FOREIGN KEY (usuario) REFERENCES usuarios (id);
 
 SET FOREIGN_KEY_CHECKS=1;
-
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.1.34-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win32
--- HeidiSQL Versión:             10.1.0.5464
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- Volcando estructura para tabla soccermoms.archivo_detalles
-
--- Volcando datos para la tabla soccermoms.archivo_detalles: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `archivo_detalles` DISABLE KEYS */;
-/*!40000 ALTER TABLE `archivo_detalles` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
