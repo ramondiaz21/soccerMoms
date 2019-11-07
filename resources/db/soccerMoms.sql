ff0016afca6b220d9401e8922bd6da42efb7a060
Ramón Díaz <jramondiaz96@gmail.com>
2019-11-07 13:39:30 -0600

    usuario administrador

--- resources/db/soccerMoms.sql
+++ resources/db/soccerMoms.sql
@@ -99,25 +99,5 @@ ALTER TABLE equipos
 
 SET FOREIGN_KEY_CHECKS=1;
 
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.1.34-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win32
--- HeidiSQL Versión:             10.1.0.5464
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- Volcando estructura para tabla soccermoms.archivo_detalles
-
--- Volcando datos para la tabla soccermoms.archivo_detalles: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `archivo_detalles` DISABLE KEYS */;
-/*!40000 ALTER TABLE `archivo_detalles` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+INSERT INTO usuarios (username, password,nombre,rol)
+VALUES ('admin',MD5('123'),'administrador',1);
